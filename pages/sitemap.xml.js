46f616a54174d16c048777d22edbeb41af8abb3b
Stackofsugar <digno.christopher@gmail.com>
2023-08-13 22:33:35 +0700

    Added sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,36 @@
+const ROOT_SITE = "https://www.stackofsugar.com";
+
+const pages = ["/", "/projects", "/hire", "/blog"];
+
+function generateSiteMap(posts) {
+    return `
+        <?xml version="1.0" encoding="UTF-8"?>
+            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+                ${pages
+                    .map((item) => {
+                        return `
+                        <url>
+                            <loc>${ROOT_SITE}${item}</loc>
+                        </url>
+                    `;
+                    })
+                    .join("")}
+            </urlset>
+`.trim();
+}
+
+function SiteMap() {}
+
+export async function getServerSideProps({ res }) {
+    const sitemap = generateSiteMap();
+
+    res.setHeader("Content-Type", "text/xml");
+    res.write(sitemap);
+    res.end();
+
+    return {
+        props: {},
+    };
+}
+
+export default SiteMap;
